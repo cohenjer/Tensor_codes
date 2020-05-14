aa851e0c74ce4e35485e2ba48a6416f3f05273fd
COHEN Jeremy <jeremy.cohen@irisa.fr>
2020-05-14 09:57:22 +0200

    Updated prox l1 and tests

--- Dictionary-based_decomposition/ProxOp/prox_ml1.m
+++ Dictionary-based_decomposition/ProxOp/prox_ml1.m
@@ -1,6 +1,7 @@
 function [U,t, nu_t] = prox_ml1(X,l,tol)
 % U is the prox of max_i \|X\|_1, t the value of the max.
-% Input X is a matrix, l the lambda parameter. tol is the desired precision.
+% Input X is a matrix, l the lambda parameter. tol is the desired
+% precision, and the stopping criterion will be tmax*tol.
 
 
 [n,m] = size(X);
@@ -12,14 +13,15 @@ Xsum = sum(Xsort,1);
 Xcumsum = cumsum(Xsort);
     % maximal value of t (no thresholding)
 tmax = max(Xsum);
-tmin = 0;
+delta = tol;
+tmin = 0; 
+t = tmax/2;
 %tol = 1e-8; % tolerance on estimation of sum(nu_i)
     % Order of visited columns of X
-[Xsumsort,order] = sort(Xsum,'descend');
-t = sum( abs( ST( X(:,order(1) ),l) ) ); %first value
+[sorted_sums, order] = sort(Xsum,'descend');
+%t = sum( abs( ST( X(:,order(1) ),l) ) ); %first value
     % Tools for computations later on
 ps = linspace(1,n,n)';
-Ps = repmat(ps, 1, m);
 
 % Remove zero solutions (bugged otherwise)
 if l>=sum(Xsort(1,:))
@@ -28,77 +30,54 @@ if l>=sum(Xsort(1,:))
     nu_t = Xsort(1,:)/l;
     return
 end
-% Loop on number of considered columns
-for i=1:m
-   % Selecting columns
-   %Xs = X(:,order(1:i)); % Thresholded columns
-   
-   % Computing the value of max_{j<=i} \|U_j\|_1 
-   % with U_j = S_{\lambda nu}(X_j)
-   loop = true;
-   while loop 
-        %fprintf('looping...')
-        % 1/ Computing the dual variables
-               
-            % cummulative sum of x and preprocessing
-            Xcumsum_m_t = (Xcumsum-t)/l;
 
-            % Compute the candidate nu values
-            Nu = Xcumsum_m_t./Ps;
-           
-            % Test to keep the only good one
-            % Check which nu is compatible with the Soft Thresholding number of nonzero
-            % coeffs p.
-            nu_t = zeros(1,m);
-            for j=1:m % O(nm) in worst case :(
-                % TODO: There has to be a better way since Xsort is sorted !!
-                for p=n:-1:1
-                    % does xsort(p) < \lambda\nu(p) < xsort(p+1)
-                    if p==n % This is often the result is early stages where all is clipped
-                        if Xsort(p,j) >= l*Nu(p,j)
-                            nu_t(j) = Nu(p,j);
-                            break
-                        end
-                    else 
-                        if Xsort(p,j) >= l*Nu(p,j) && l*Nu(p,j) > Xsort(p+1,j)
-                            nu_t(j) = Nu(p,j);
-                            break
-                        end %end if
-                    end %end if p==n
-                end %end for p
-            end %end for j
-       
-        
-        % 2/ Checking dual feasibility
-        if abs(sum(nu_t)-1)<tol
-           loop = false;
-        end
-        % 3/ Checking dual condition, 1< or 1>
-        if sum(nu_t) < 1 && loop
-            % t must be decreased
-            tmax = t;
-            t = (t + tmin)/2;
-        elseif loop
-            % t must be increased
-            tmin = t;            
-            t = (t + tmax)/2;
-        end
-   end
-   
-   % Checking primal feasibility
-   if i==m || t>Xsumsort(i+1) 
-       % The right solution has been found, now we only need to clip these
-       % vectors
-       break
-   end
-   
-   % Resetting the tmax variable for next loop
-   % Indeed tmin will only increase from the t value
-   tmax = max(Xsum);
-   tmin = t; 
-   %tmin = 0;
-   
-end % loop on thresholded vectors
+% Bisection
+while tmax - tmin > delta 
+    %fprintf('looping...')
+    % 1/ Computing the current active set
+    I = order(sorted_sums>t);
+    i = length(I); %todo improve
+    
+    % cummulative sum of x and preprocessing
+    Xcumsum_m_t = (Xcumsum(:,order(1:i))-t)/l;
+    
+    % Compute the candidate nu values (warning: already sorted)
+    Ps = repmat(ps, 1, i);
+    Nu = Xcumsum_m_t./Ps;
+    
+    % Test to keep the only good one
+    % Check which nu is compatible with the Soft Thresholding number of nonzero
+    % coeffs p.
+    nu_t = zeros(1,m);
+    for j=1:i % O(nm) in worst case :(
+        % TODO: There has to be a better way since Xsort is sorted !!
+        for p=n:-1:1
+            % does xsort(p) < \lambda\nu(p) < xsort(p+1)
+            if p==n % This is often the result is early stages where all is clipped
+                if Xsort(p,order(j)) >= l*Nu(p,j)
+                    nu_t(order(j)) = Nu(p,j);
+                    break
+                end
+            else
+                if Xsort(p,order(j)) >= l*Nu(p,j) && l*Nu(p,j) > Xsort(p+1,order(j))
+                    nu_t(order(j)) = Nu(p,j);
+                    break
+                end %end if
+            end %end if p==n
+        end %end for p
+    end %end for j
+    
+    % 3/ Checking dual condition, 1< or 1>
+    if sum(nu_t) < 1 
+        % t must be decreased
+        tmax = t;
+        t = (t + tmin)/2;
+    else
+        % t must be increased
+        tmin = t;
+        t = (t + tmax)/2;
+    end
+end
 
 % Final step, thresholding vectors that need to be
 U = X;
--- Dictionary-based_decomposition/ProxOp/testfile_proxl11.m
+++ Dictionary-based_decomposition/ProxOp/testfile_proxl11.m
@@ -1,20 +1,30 @@
 %% Script for testing the proximal operator of the matrix induced l1 norm
 
-for n=1:100
-    n
-    for m=1:100
+% Dummy test
+%toto = [ 3, 2, 1, 4.1 ; 2, 1, 0 , 3; 1, 0, 0, 2 ];
+toto = randn(100,10);
+tic
+[U,t, nu]= prox_ml1(toto,10,10^-8);
+
+
+%%
+clear out
+
+for n=1:10
+    100*n
+    for m=1:10
         temp = zeros(1,5);
         for i=1:5
             % Parameters
-            lamb = 1;
+            lamb = 10;
             % Data
-            X = randn(10*n,m);
+            X = randn(100*n,10*m);
             % Prox vs sum
             clock_alg=tic;
             prox_ml1(X,lamb,10^(-8));
             t=toc(clock_alg);
             % output
-            temp(i) = t/(m^2*10*n);
+            temp(i) = t/(10*m*100*n*log2(100*n));
             clear clock_alg
             clear clock_ref
         end
@@ -22,12 +32,13 @@ for n=1:100
     end
 end
 
-surf(10:10:1000,1:100,log10(out)')
+surf(100:100:1000,10:10:100,log10(out)')
+axis([100 1000 10 100])
 xlabel('n')
 ylabel('m')
-zlabel('log_{10}(t/nm^2)')
+zlabel('log_{10}(t/nm log_2(n))')
 title('')
 view(0,90)
 c = colorbar;
-c.Label.String = 'log_{10}(t/nm^2)';
+c.Label.String = 'log_{10}(t/nm log_2(n))';
 %title('Effective computation time of prox of l11')
